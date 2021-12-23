a8f5af67bf1e56084eb93d88b444534f7cae1134
h496022202 <h496022202@gmail.com>
2021-12-23 23:02:37 +0800

    Update start_atc.sh

--- start_atc.sh
+++ start_atc.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
-atcd --atcd-wan eth0 --atcd-lan wlan0 &
 
-atcd &
+iptables-restore < etc/iptables.ipv4.nat
 
+atcd --atcd-wan eth0 --atcd-lan wlan0 >atcd.log & 
 
+sleep 5
 
-python ~/atcui/manage.py runserver 0.0.0.0:8000 &
-sh ~/augmented-traffic-control/utils/restore-profiles.sh localhost:8080
+python ../atcui/manage.py runserver 0.0.0.0:8000 > atcui.log &
+
+sleep 5
+
+cd ../augmented-traffic-control/utils/
+sh restore-profiles.sh localhost:8080
 

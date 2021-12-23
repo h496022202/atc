ee05af1e6d54535e8d57710164bb0c31c25081e4
h496022202 <h496022202@gmail.com>
2021-12-23 20:28:02 +0800

    Create create_apd.sh

---
+++ create_apd.sh
@@ -0,0 +1,65 @@
+#/bash/bin
+
+APPASS="$1"
+APSSID="rPi3"
+
+if [[ $# -eq 2 ]]; then
+	APSSID=$2
+fi
+
+apt-get remove --purge hostapd -yqq
+apt-get update -yqq
+apt-get upgrade -yqq
+apt-get install hostapd dnsmasq -yqq
+
+cat > /etc/dnsmasq.conf <<EOF
+interface=wlan0
+listen-address=10.9.0.1
+bind-interfaces
+server=8.8.8.8
+domain-needed
+bogus-priv
+dhcp-range=10.9.0.2,10.9.0.50,255.255.255.0,12h
+EOF
+
+cat > /etc/hostapd/hostapd.conf <<EOF
+interface=wlan0
+hw_mode=g
+channel=10
+auth_algs=1
+wpa=2
+wpa_key_mgmt=WPA-PSK
+wpa_pairwise=CCMP
+rsn_pairwise=CCMP
+wpa_passphrase=$APPASS
+ssid=$APSSID
+ieee80211n=1
+wmm_enabled=1
+ht_capab=[HT40][SHORT-GI-20][DSSS_CCK-40]
+EOF
+
+sed -i -- 's/allow-hotplug wlan0//g' /etc/network/interfaces
+sed -i -- 's/iface wlan0 inet manual//g' /etc/network/interfaces
+sed -i -- 's/    wpa-conf \/etc\/wpa_supplicant\/wpa_supplicant.conf//g' /etc/network/interfaces
+sed -i -- 's/#DAEMON_CONF=""/DAEMON_CONF="\/etc\/hostapd\/hostapd.conf"/g' /etc/default/hostapd
+
+cat >> /etc/network/interfaces <<EOF
+# Added by rPi Access Point Setup
+allow-hotplug wlan0
+iface wlan0 inet static
+	address 10.9.0.1
+	netmask 255.255.255.0
+	network 10.9.0.0
+	broadcast 10.9.0.255
+
+EOF
+
+echo "denyinterfaces wlan0" >> /etc/dhcpcd.conf
+
+systemctl enable hostapd
+systemctl enable dnsmasq
+
+sudo service hostapd start
+sudo service dnsmasq start
+
+echo "All done! Please reboot"
